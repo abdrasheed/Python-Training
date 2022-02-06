4dc318cee80ada3971a352831d6bbd03ac2fa0b8
Abdelrahman Alarqan <abdalarqan20@gmail.com>
2022-02-06 15:36:18 +0200

    initial commit

---
+++ syntax.py
@@ -0,0 +1,33 @@
+
+
+'''
+------->>> VARIABLE <<<-------
+int = 10
+str = "hello"
+bool = true
+float = 10.5
+tuple = (1,2,3)
+list = [1,2,3]
+dict = {"name" : "ahmed" , "age" : 25}
+x,y,z = 1,2,3
+
+--->>> ESCAPE CHARACTERS ESCAPE CHARACTERS <<<--
+\b => Back Space \\ print("ESCAPE\bCHARACTERS") => will remove E char.
+\  => single line \\ print("ESCAPE \
+                        CHARACTERS") => will print to words in single line.
+\\ => Print slash \\ print("ESCAPE CHARACTERS\\") will print one slash.
+\n => New line \\ print("ESCAPE \n CHARACTERS") will move to new line.
+\t => Tap \\ print("tap\tCHARACTERS") will take tap
+------->>> STRINGS <<<-------
+
+
+
+------->>> FUNCTIONS <<<-------
+type(10) --> it will return the type of data
+'''
+
+text = '''
+here we ge "ss"
+'adsa'
+'''
+print(text)
